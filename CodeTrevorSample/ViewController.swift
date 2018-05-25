99e48fdf124490564a26966c13c1a4cbd2376fcb
Trevor Stevenson <trevor.stevenson@duke.edu>
2018-05-24 21:59:31 -0400

    Add Pie chart

--- CodeTrevorSample/ViewController.swift
+++ CodeTrevorSample/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CodeTrevor
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let chart = Pie(frame: CGRect(x: view.frame.size.width/2-100, y: view.frame.size.height/2-100, width: 200, height: 200), sections: 3, radius: 100)
+        view.addSubview(chart)
     }
 
     override func didReceiveMemoryWarning() {
