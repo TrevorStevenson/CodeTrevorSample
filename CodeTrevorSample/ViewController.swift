d4a7f072240918e80e720f65887c86a0c4415f4a
Trevor Stevenson <trevor.stevenson@duke.edu>
2018-05-29 14:21:38 -0400

    Add responsive ui to button

--- CodeTrevorSample/ViewController.swift
+++ CodeTrevorSample/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import CodeTrevor
 
 class ViewController: UIViewController {
+    
+    var chart: Pie = Pie()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,21 +19,28 @@ class ViewController: UIViewController {
         
         view.createBackground(color: UIColor.soothingBreeze)
         
-        let chart = Pie(center: CGPoint(point: view.center, offsetX: 0, offsetY: -50), radius: 150, values: [2, 4, 2, 8, 10])
+        chart = Pie(center: CGPoint(point: view.center, offsetX: 0, offsetY: -50), radius: 150, values: [2, 4, 2, 8, 10])
         chart.labels = ["Bitcoin", "Ethereum", "Litecoin", "Bitcoin Cash", "TrevCoin"]
+        
         if let openSans = UIFont(name: "OpenSans-Regular", size: 15) {
             chart.labelFont = openSans
         }
 
         view.addSubview(chart)
         
-        chart.scaleUp(withDuration: 0.2)
+        chart.scaleUp(withDuration: 0.1)
         
         let button = TrevButton(center: CGPoint(x: view.centerX, y: view.height-80), width: 180, height: 40)
         button.color = UIColor.electronBlue
         button.setTitle("Button", for: .normal)
         button.titleLabel?.font = UIFont(name: "OpenSans-Regular", size: 20)
+        button.addTarget(self, action: #selector(self.buttonPressed), for: .touchUpInside)
         view.addSubview(button)
+    }
+    
+    @objc func buttonPressed()
+    {
+        chart.scaleUp(withDuration: 0.1)
     }
 
     override func didReceiveMemoryWarning() {
